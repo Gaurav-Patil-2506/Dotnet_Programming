813484536e7d19a9cf7eb82e0a4ee1ab5aa98734
D2-83689-Gaurav <gpatil873@gmail.com>
2024-05-15 18:45:14 +0530

    Assignment1 completed

---
+++ Assignment1/Question2/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Enter value of n1");
+            //String n1value= Console.ReadLine();
+            int n1 = Convert.ToInt32(args[0]);
+
+            //Console.WriteLine("Enter Value for n2");
+            //string n2Value = Console.ReadLine();
+            int n2 = Convert.ToInt32(args[1]);
+
+            //Console.WriteLine("Enter Choice");
+            string choice = args[2];
+
+            switch (choice)
+            {
+                case "+":
+                    Console.WriteLine("addition= " + (n1 + n2));                    
+                    break;
+                case "-":
+                    Console.WriteLine("Substration= " + (n1 - n2));
+                    break;
+                case "*":
+                    Console.WriteLine("Multiplication= " + (n1* n2));
+                    break;
+                case "/":
+                    Console.WriteLine("Division= " + (n1 / n2));
+                    break;                
+                default:
+                    Console.WriteLine("Wrong choice");
+                    break;
+            }
+
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Assignment1/Question3/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question3
+{
+    internal class Program
+    {
+        static int plusMethod(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+
+        static int substractMethod(int n1, int n2)
+        {
+            return n1 - n2;
+        }
+        static int multiplyMethod(int n1, int n2)
+        {
+            return n1 * n2;
+        }
+
+        static int divisionMethod(int n1, int n2)
+        {
+            return n1 / n2;
+        }
+        static void Main(string[] args)
+        {
+            
+            int choice;
+            
+            do
+            {           
+                
+                Console.WriteLine("0.Exit");
+                Console.WriteLine("1.Addition");
+                Console.WriteLine("2.Substraction");
+                Console.WriteLine("3.Multiply");
+                Console.WriteLine("4.Divison");
+                Console.WriteLine("Enter choice");
+                string choiceValue = Console.ReadLine();
+                choice = Convert.ToInt32(choiceValue);
+
+                Console.WriteLine("Enter value of n1");
+                String n1value = Console.ReadLine();
+                int n1 = Convert.ToInt32(n1value);
+
+                Console.WriteLine("Enter Value for n2");
+                string n2Value = Console.ReadLine();
+                int n2 = Convert.ToInt32(n2Value);
+
+                switch (choice)
+                {
+                    case 0: Console.WriteLine("exit");
+                        
+                        break;
+                    case 1: Console.WriteLine("Addition- ");
+                        Console.WriteLine(plusMethod(n1, n2)); 
+                        
+                        break;
+                    case 2: Console.WriteLine("Substraction- ");
+                        Console.WriteLine(substractMethod(n1, n2));
+                        break;
+                    case 3: Console.WriteLine("Multiplication- ");
+                        Console.WriteLine(multiplyMethod(n1, n2));
+                        break;
+                    case 4: Console.WriteLine("Division- ");
+                        Console.WriteLine(divisionMethod(n1, n2));
+                        break;
+
+                }
+                
+
+            } while (choice!=0);
+        }
+    }
+}
